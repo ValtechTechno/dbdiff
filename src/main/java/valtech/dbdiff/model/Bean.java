c429804e5b2ed47b1113df2878a3016a3298e7bc
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2013-01-17 11:49:37 +0100

    add: attribute, fail ORA-20000: There was 2 difference(s) [ERROR] -- - column - BEAN.VALUE [ERROR] ++ - type - BEAN.VALUE VARCHAR2(1020--) null=Y

--- src/main/java/valtech/dbdiff/model/Bean.java
+++ src/main/java/valtech/dbdiff/model/Bean.java
@@ -9,11 +9,21 @@ public class Bean {
 	@Id
 	private int id;
 
+	private String value;
+
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 }
